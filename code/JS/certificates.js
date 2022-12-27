32597b5ac711500a1fc1594c92fe2751aaed81a5
NilsonDeon <nilsondeon01@gmail.com>
2022-12-27 16:48:33 -0300

    skills-certificates-finished

--- code/JS/certificates.js
+++ code/JS/certificates.js
@@ -2,45 +2,29 @@ const swiper = new Swiper('.swiper', {
 
     direction: 'horizontal',
     loop: true,
-    
-    slidesPerView: 1,
-    spaceBetween: 200,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
 
     breakpoints: {
+
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 200,
+      },
+
         640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
+          slidesPerView: 2,
+          spaceBetween: 20,
         },
 
         1024: {
-            slidesPerView: 3,
-            spaceBetween: 50,
+          slidesPerView: 3,
+          spaceBetween: 20,
         },
     },
-
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
 
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
-    autoplay: {
-        delay: 5000,
-        pauseOnMouseEnter: true,
-      },
-  
-
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    }
 
   });
   
